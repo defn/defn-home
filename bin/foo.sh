d164f67ce1b80824528300a7b810b438b920bd30
Tom Bombabil <amanibhavam@destructuring.org>
2015-11-16 22:17:02 -0800

    localhost ssh key for cache

--- bin/foo.sh
+++ bin/foo.sh
@@ -2,8 +2,13 @@
 
 function main {
   set -x
+
+  echo 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDfchHJtADoZClQITn1qFK7Jy7B9tje/dCFLzrqpdmOyUhmz9zGTjwmL7hXxuyPZgAcdrGPKE+3DjpqqEIxfem1hta0Ej6O8ad/9z0x+xBe2G1EFADYi5gdHuU3+Fh/hUUDWKnQKO0UlAewk488yIUYUU+UVcaPXr9pF1U5VfW8bue76ML9sdR9qIGP2Va+bJhX8neMffc79ShEd02kqVg0kSeQduCK5rNg51wWVNoCe6gvkufuAGvwVOh/jWwGvEsOMcqOFUipKi1ltCqu8vwheTSzLxGUuOUKBedxFjCip0xeZzCP+hFxxS56FaqcRqrsoLTQdWOWduRrWNFlXyxn defn@spiral.local' >> ~/.ssh/authorized_keys
+
   ssh -oStrictHostKeyChecking=no github.com true
   ssh -oStrictHostKeyChecking=no bitbucket.org true
+  ssh -oStrictHostKeyChecking=no localhost true
+
   git clone git@github.com:defn/home home
   mv home/.git .
   git reset --hard
