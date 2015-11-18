0558d0cf80b99a1bc5614ccf36b6b9d6834dca94
Tom Bombabil <amanibhavam@destructuring.org>
2015-11-17 17:49:01 -0800

    set up environment before calling profile in vagrant

--- bin/foo.sh
+++ bin/foo.sh
@@ -3,6 +3,8 @@
 function main {
   set -x
 
+  cd
+
   echo 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDfchHJtADoZClQITn1qFK7Jy7B9tje/dCFLzrqpdmOyUhmz9zGTjwmL7hXxuyPZgAcdrGPKE+3DjpqqEIxfem1hta0Ej6O8ad/9z0x+xBe2G1EFADYi5gdHuU3+Fh/hUUDWKnQKO0UlAewk488yIUYUU+UVcaPXr9pF1U5VfW8bue76ML9sdR9qIGP2Va+bJhX8neMffc79ShEd02kqVg0kSeQduCK5rNg51wWVNoCe6gvkufuAGvwVOh/jWwGvEsOMcqOFUipKi1ltCqu8vwheTSzLxGUuOUKBedxFjCip0xeZzCP+hFxxS56FaqcRqrsoLTQdWOWduRrWNFlXyxn defn@spiral.local' >> ~/.ssh/authorized_keys
 
   ssh -oStrictHostKeyChecking=no github.com true
@@ -16,6 +18,8 @@ function main {
   git clean -fd
 
   script/bootstrap || true
+  source work/jq/script/profile
+  source work/app/script/profile
   source script/profile
   require
   app bootstrap
