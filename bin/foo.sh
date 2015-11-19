82ee34950e32fd525e4f50e9a9a2e4b276b15dc8
Tom Bombabil <amanibhavam@destructuring.org>
2015-11-18 22:09:16 -0800

    add comments to vagrant provisioning

--- bin/foo.sh
+++ bin/foo.sh
@@ -1,34 +1,44 @@
 #!/usr/bin/env bash
 
 function main {
-  set -x
+  # copy cache files
+  if [[ -d /vagrant ]]; then
+    pushd /vagrant
+    rsync -ia distfiles cache packages ~/
+    popd
+  fi
 
+  # make sure we are at home
+  set -x
   cd
 
+  # let myself in
   echo 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDfchHJtADoZClQITn1qFK7Jy7B9tje/dCFLzrqpdmOyUhmz9zGTjwmL7hXxuyPZgAcdrGPKE+3DjpqqEIxfem1hta0Ej6O8ad/9z0x+xBe2G1EFADYi5gdHuU3+Fh/hUUDWKnQKO0UlAewk488yIUYUU+UVcaPXr9pF1U5VfW8bue76ML9sdR9qIGP2Va+bJhX8neMffc79ShEd02kqVg0kSeQduCK5rNg51wWVNoCe6gvkufuAGvwVOh/jWwGvEsOMcqOFUipKi1ltCqu8vwheTSzLxGUuOUKBedxFjCip0xeZzCP+hFxxS56FaqcRqrsoLTQdWOWduRrWNFlXyxn defn@spiral.local' >> ~/.ssh/authorized_keys
 
+  # initialize new ssh hosts
   ssh -oStrictHostKeyChecking=no github.com true
   ssh -oStrictHostKeyChecking=no bitbucket.org true
   ssh -oStrictHostKeyChecking=no localhost true
 
+  # clone home
   git clone git@github.com:defn/home home
   mv home/.git .
   git reset --hard
   rm -rf home
   git clean -fd
 
+  # bootstrap app
   git clone git@github.com:defn/app work/app
   pushd work/app
   script/bootstrap
-  set +x
-  source script/profile
-  set -x
+  set +x; source script/profile; set -x
   popd
 
+  # bootstrap home
   script/bootstrap
-  set +x
-  source script/profile
-  set -x
+  set +x; source script/profile; set -x
+
+  # bootstrap everything
   app bootstrap
 }
 
