a7d594253296b1eacb4964a7f7ccd95779fd88e8
Tom Bombadil <amanibhavam@destructuring.org>
2016-04-04 12:47:21 -0700

    use jenkin service, move apt-mirror to that block

--- server.cljs
+++
@@ -1,8 +0,0 @@
-(def express (nodejs/require "express"))
-(def serve-static (nodejs/require "serve-static"))
-
-(defn -main []
-  (let [app (express)]
-    (.use app (serve-static "/vagrant/cache/apt-mirror/mirror/archive.ubuntu.com/"))
-    (.listen app 3000 (fn []
-                        (println "Server started on port 3000")))))
