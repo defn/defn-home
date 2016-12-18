a7828f07821c2a4d208e96afc8a668b4f5b69c53
Tom Bombadil <amanibhavam@destructuring.org>
2016-12-18 11:21:29 -0800

    open -g hammerspoon://meh

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -1,3 +1,7 @@
 hs.hotkey.bind({"shift", "cmd"}, "W", function()
   hs.notify.new({title="Hammerspoon", informativeText="Hello World"}):send()
 end)
+
+hs.urlevent.bind("meh", function(event_name, params)
+  hs.notify.new({title="meh", informativeText="meh..."}):send()
+end)
